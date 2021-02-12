ef0007cee84e0c6951fd95853f92f9a7feb625f4
Guo Xiaochen <77771793+SilLea@users.noreply.github.com>
2021-02-12 21:28:38 +0800

    Update UVA10340 All In All.cpp
    一个简单的字符串处理问题

--- questions/UVA10340 All In All.cpp
+++ questions/UVA10340 All In All.cpp
@@ -1,31 +1,31 @@
-//#include<stdio.h>
-//#include<string.h>
-//#include<ctype.h>
-//char ch2[100000],ch1[100000];
-//int main(void)
-//{
-//	while (scanf("%s%s", ch1, ch2) != EOF) 
-//	{
-//		int i, j = 0;
-//		int len1 = strlen(ch1);
-//		int len2 = strlen(ch2);
-//		for (i = 0, j = 0; i < len1; i++)
-//		{
-//			for (; j <= len2;)
-//			{
-//				if (ch1[i] != ch2[j])
-//					j++;
-//				else 
-//				{
-//					j++;
-//					break;
-//				}
-//			}
-//			if (j > len2)break;
-//		}
-//		if (j <= len2 && i==len1)
-//			printf("Yes\n");
-//		else
-//			printf("No\n");
-//	}
-//}
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+char ch2[100000],ch1[100000];
+int main(void)
+{
+	while (scanf("%s%s", ch1, ch2) != EOF) 
+	{
+		int i, j = 0;
+		int len1 = strlen(ch1);
+		int len2 = strlen(ch2);
+		for (i = 0, j = 0; i < len1; i++)
+		{
+			for (; j <= len2;)
+			{
+				if (ch1[i] != ch2[j])
+					j++;
+				else 
+				{
+					j++;
+					break;
+				}
+			}
+			if (j > len2)break;
+		}
+		if (j <= len2 && i==len1)
+			printf("Yes\n");
+		else
+			printf("No\n");
+	}
+}
