cf07bb7e05042262f53b5971ea9997876d4c7dca
Guo Xiaochen <77771793+SilLea@users.noreply.github.com>
2021-04-19 23:25:23 +0800

    字典树, 算法优化, 斐波那契数列

---
+++ questions/UVA12333 Fibonacci的复仇 Revenge of Fibonacci.cpp
@@ -0,0 +1,98 @@
+/*
+    UVA 12333
+
+    by - SilLea  2021/4/16
+*/
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<string>
+using namespace std;
+const int limit = 40;
+int ID[256], F[2][22000], cnt, present[42];
+typedef struct node1
+{
+    int    id;
+    int    flag;
+    node1* v[10];
+} tnode;
+tnode  dict[4000004];
+tnode* root;
+tnode* newnode()
+{
+    return &dict[cnt++];
+}
+void TreeInsert(int* A, int len, int id)
+{
+    tnode* u = root;
+    for (int i = 0; i < len; i++)
+    {
+        if (!u->v[A[i]])
+            u->v[A[i]] = newnode();
+        if (!u->id && !u->flag)
+            u->id = id;
+        u = u->v[A[i]];
+    }
+    if (!u->flag)
+    {
+        u->flag = 1;
+        u->id = id;
+    }
+}
+void initial()
+{
+    for (int i = 0; i < 10; i++)
+        ID[i + '0'] = i;
+    memset(F, 0, sizeof(F));
+    int s = 0, l = 1, r, cont, p, q;
+    F[0][0] = F[1][0] = 1;
+    root = newnode();
+    TreeInsert(F[0], 1, 0);
+    for (int i = 2; i < 100000; i++)
+    {
+        p = i & 1, q = (i + 1) & 1;
+        for (int j = s; j < l; j++)
+        {
+            F[p][j] = F[p][j] + F[q][j];
+        }
+        for (int j = s; j < l; j++)
+        {
+            if (F[p][j] >= 10)
+            {
+                F[p][j + 1] += 1;
+                F[p][j] -= 10;
+            }
+        }
+        if (F[p][l])l++;
+        if (l - s > 51)s++;
+        r = l - 1, cont = 0;
+        while (cont < 40 && r >= 0)
+            present[cont++] = F[p][r--];
+        TreeInsert(present, cont, i);
+    }
+}
+int SearchTree(string& str)
+{
+    tnode* start = root;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (!start->v[ID[str[i]]])
+            return -1;
+        else
+            start = start->v[ID[str[i]]];
+    }
+    return start->id;
+}
+int main()
+{
+    initial();
+    int t;
+    string frt;
+    cin >> t;
+    for (int kase = 1; kase <= t; kase++)
+    {
+        cin >> frt;
+        cout << "Case #" << kase << ": " <<SearchTree(frt) << endl;
+    }
+    return 0;
+}
