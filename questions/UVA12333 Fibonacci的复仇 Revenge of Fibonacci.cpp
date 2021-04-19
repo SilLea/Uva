e8bd3d6a09baac2cb2aa5a9bdea637332f799338
Guo Xiaochen <77771793+SilLea@users.noreply.github.com>
2021-04-19 23:35:16 +0800

    字典树, 大数相加, 斐波那契数列

--- questions/UVA12333 Fibonacci的复仇 Revenge of Fibonacci.cpp
+++ questions/UVA12333 Fibonacci的复仇 Revenge of Fibonacci.cpp
@@ -1,11 +1,7 @@
 /*
     UVA 12333
 
-<<<<<<< HEAD
-    by - SilLea  2021/4/16
-=======
     by - SilLea  2021/4/17
->>>>>>> 192255d499290c92562a39f0bc57a2e5e26fb721
 */
 #include<iostream>
 #include<algorithm>
@@ -20,15 +16,9 @@ typedef struct node1
     int    flag;
     node1* v[10];
 } tnode;
-<<<<<<< HEAD
-tnode  dict[4000004];
-tnode* root;
-tnode* newnode()
-=======
 tnode  dict[4000004];//全部节点
 tnode* root;//根
 tnode* newnode()//创造节点
->>>>>>> 192255d499290c92562a39f0bc57a2e5e26fb721
 {
     return &dict[cnt++];
 }
@@ -37,19 +27,11 @@ void TreeInsert(int* A, int len, int id)
     tnode* u = root;
     for (int i = 0; i < len; i++)
     {
-<<<<<<< HEAD
-        if (!u->v[A[i]])
-            u->v[A[i]] = newnode();
-        if (!u->id && !u->flag)
-            u->id = id;
-        u = u->v[A[i]];
-=======
         if (!u->v[A[i]])//如果没有子节点, 就创造新节点
             u->v[A[i]] = newnode();
         if (!u->id && !u->flag)//如果该节点未被赋值, 赋值
             u->id = id;
         u = u->v[A[i]];//下一个节点
->>>>>>> 192255d499290c92562a39f0bc57a2e5e26fb721
     }
     if (!u->flag)
     {
@@ -71,35 +53,20 @@ void initial()
         p = i & 1, q = (i + 1) & 1;
         for (int j = s; j < l; j++)
         {
-<<<<<<< HEAD
-            F[p][j] = F[p][j] + F[q][j];
-        }
-        for (int j = s; j < l; j++)
-        {
-            if (F[p][j] >= 10)
-=======
             F[p][j] = F[p][j] + F[q][j];//大数相加(最右边是第一位)
         }
         for (int j = s; j < l; j++)
         {
             if (F[p][j] >= 10)//进位
->>>>>>> 192255d499290c92562a39f0bc57a2e5e26fb721
             {
                 F[p][j + 1] += 1;
                 F[p][j] -= 10;
             }
         }
-<<<<<<< HEAD
-        if (F[p][l])l++;
-        if (l - s > 51)s++;
-        r = l - 1, cont = 0;
-        while (cont < 40 && r >= 0)
-=======
         if (F[p][l])l++;//长度增加
         if (l - s > 51)s++;//只取前52位相加, 优化速度且保证答案正确
         r = l - 1, cont = 0;
         while (cont < 40 && r >= 0)//将数正过来, 插入字典树
->>>>>>> 192255d499290c92562a39f0bc57a2e5e26fb721
             present[cont++] = F[p][r--];
         TreeInsert(present, cont, i);
     }
@@ -111,12 +78,7 @@ int SearchTree(string& str)
     {
         if (!start->v[ID[str[i]]])
             return -1;
-<<<<<<< HEAD
-        else
-            start = start->v[ID[str[i]]];
-=======
         start = start->v[ID[str[i]]];
->>>>>>> 192255d499290c92562a39f0bc57a2e5e26fb721
     }
     return start->id;
 }
@@ -129,11 +91,7 @@ int main()
     for (int kase = 1; kase <= t; kase++)
     {
         cin >> frt;
-<<<<<<< HEAD
-        cout << "Case #" << kase << ": " <<SearchTree(frt) << endl;
-=======
         cout << "Case #" << kase << ": " << SearchTree(frt) << endl;
->>>>>>> 192255d499290c92562a39f0bc57a2e5e26fb721
     }
     return 0;
 }
